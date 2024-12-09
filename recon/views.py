4d32e28b1853ed734f66c5766af04bdaa1edd6f1
Aaron Dguez <aarondguez@proton.me>
2024-12-09 06:12:38 -0700

    updated logs/cache from django and finished the search function with its page. Also prepared for pruebas and Arduino connection.

--- recon/views.py
+++ recon/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
+from tracking.models import Tracking
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def searchTracking(request, track: str):
-    if(track == ''):
-        return render(request, 'searchTracking.html', { 'status': 'No Tracking Number Provided' })
-    if(request.method == 'GET'):
-        pass
+@csrf_exempt
+def searchTracking(request):
+    if request.method == 'POST':
+        track = request.POST.get('track', '').strip()
+        if not track:
+            return render(request, 'busqueda.html', { 'status': 'No Tracking Number Provided' })
+        trackedOrder: Tracking = Tracking.objects.filter(trackNumber=track).first()
+        if(trackedOrder):
+            return render(request, 'busqueda.html', { 'status': 'Order Found', 'track': trackedOrder })
+        else:
+            return render(request, 'busqueda.html', { 'status': 'Order Not Found' })
     else:
-        return render(request, 'searchTracking.html', { 'status': 'Invalid Access' })
+        return render(request, 'busqueda.html', { 'status': 'Invalid Access' })
