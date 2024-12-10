e15578892c502f6f97392a73538cb6b6f21c474e
Aaron Dguez <aarondguez@proton.me>
2024-12-10 14:18:03 -0700

    .

--- pruebas/models.py
+++ pruebas/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-from tracking.models import Tracking
-from django.utils import timezone
 
 # Create your models here.
 class pruebas(models.Model):
-    id = models.AutoField(primary_key=True)
     tamano = models.BooleanField(null=False),
     color = models.BooleanField(null=False),
     relleno = models.BooleanField(null=False),
