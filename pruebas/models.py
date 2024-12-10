26c9dacbfd233d73e871770dd574de0c53c105c4
Aaron Dguez <aarondguez@proton.me>
2024-12-10 14:20:33 -0700

    .

--- pruebas/models.py
+++ pruebas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class pruebas(models.Model):
+    id = models.IntegerField(primary_key=True, serialize=False),
     tamano = models.BooleanField(null=False),
     color = models.BooleanField(null=False),
     relleno = models.BooleanField(null=False),
